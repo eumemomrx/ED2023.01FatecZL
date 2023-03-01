27c01cbd4c50550de301d429ae45334776bf7367
eumemomrx <113364863+eumemomrx@users.noreply.github.com>
2023-02-28 22:49:08 -0300

    Update Ex05.java

--- ED.FATECZL.EX03/src/controller/Ex05.java
+++ ED.FATECZL.EX03/src/controller/Ex05.java
@@ -1,29 +1,18 @@
 package controller;
 
-public class Ex06 {
-
-	public Ex06() {
+public class Ex05 {
+	public Ex05() {
 		super();
 	}
-	public double RecD(int n){
-		double f;
-		if(n==1){
-			return 1;
-		}else{
-			f = 1/(double)FFat(n)+ RecD(n-1);
-			return(f);
-		}
+	public int Fibo(int n){
+                if(n>20){
+                	return 0;
+                }
+                if(n==1 || n==2){
+			//Condição de parada, que é o valor ser igual a 1 ou 2
+                	return 1;
+              	}else{
+			//Caso nao, retorna a soma de duas chamadas da função, com o valor -1 e -2
+			return Fibo(n-1)+Fibo(n-2);
 	}
-	public int FFat(int n) {
-		int x =0;
-		if(n==1) {
-			//Condição de parada, que é chegar no fim da conta(multiplicação por 1)
-			return 1;
-		}else {
-			x+= n*FFat(n-1);
-			//Chama a proxima recursiva com o valor - 1 para realizar a multiplicação
-		}
-		return x;
-	}
-
 }
